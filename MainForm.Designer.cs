72ab1b5790e7d0610a9f6c20d58ba92d654973f2
Răzvan Trufin <144681144+razvantrufin99xx@users.noreply.github.com>
2024-06-05 01:45:43 +0300

    Add files via upload

---
+++ MainForm.Designer.cs
@@ -0,0 +1,142 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: razvan
+ * Date: 6/4/2024
+ * Time: 2:23 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace HeadTailCoinFlip
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.TextBox textBox6;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.textBox6 = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(568, 32);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(155, 150);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Flip Coin";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox1.Location = new System.Drawing.Point(50, 32);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(512, 150);
+			this.textBox1.TabIndex = 1;
+			this.textBox1.Text = "flipcoin";
+			this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(50, 188);
+			this.textBox2.Multiline = true;
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(673, 271);
+			this.textBox2.TabIndex = 2;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox3.Location = new System.Drawing.Point(50, 465);
+			this.textBox3.Multiline = true;
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(336, 150);
+			this.textBox3.TabIndex = 3;
+			this.textBox3.Text = "head";
+			this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			// 
+			// textBox4
+			// 
+			this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox4.Location = new System.Drawing.Point(392, 465);
+			this.textBox4.Multiline = true;
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(331, 150);
+			this.textBox4.TabIndex = 4;
+			this.textBox4.Text = "tail";
+			this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(14, 475);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(42, 20);
+			this.textBox5.TabIndex = 5;
+			this.textBox5.Text = "head";
+			// 
+			// textBox6
+			// 
+			this.textBox6.Location = new System.Drawing.Point(716, 475);
+			this.textBox6.Name = "textBox6";
+			this.textBox6.Size = new System.Drawing.Size(42, 20);
+			this.textBox6.TabIndex = 6;
+			this.textBox6.Text = "tail";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(790, 633);
+			this.Controls.Add(this.textBox6);
+			this.Controls.Add(this.textBox5);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button1);
+			this.Name = "MainForm";
+			this.Text = "HeadTailCoinFlip";
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ MainForm.cs
@@ -0,0 +1,58 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: razvan
+ * Date: 6/4/2024
+ * Time: 2:23 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace HeadTailCoinFlip
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void MainFormLoad(object sender, EventArgs e)
+		{
+	
+		}
+		int i = 0;
+		int couterHead = 0;
+		int couterTail = 0;
+		public void run()
+		{
+			Random r = new Random();
+			int x = r.Next(0,1000);
+			if(x%2==0){textBox1.Text="   TAIL   "; couterTail++;}
+			else if(x%2!=0){textBox1.Text = "   HEAD   "; couterHead++;}
+			else {textBox1.Text = "    ?????   ";;}
+			//textBox2.Text +=textBox1.Text + " >> " + (i).ToString() + " -- h=" + couterHead.ToString() + ", t="+ couterTail.ToString()+" ;" ;
+			textBox2.Text +=textBox1.Text + ">>" + (i).ToString()+": ";
+			textBox3.Text = couterHead.ToString();
+			textBox4.Text = couterTail.ToString();
+			i++;
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+			run();
+			
+		}
+	}
+}
